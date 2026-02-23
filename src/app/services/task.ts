38777aebe1fa31148038d868d1d47b910fed1d4e
unknown <jorgegarnelolopez@gmail.com>
2026-02-23 09:42:45 +0100

    Front actualizado para enviar el token JWT en las peticiones

--- src/app/services/task.ts
+++ src/app/services/task.ts
@@ -1,29 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-providedIn: 'root'
+  providedIn: 'root'
 })
 export class TaskService {
-private apiUrl = 'http://localhost:3000/api/tareas';
+  private apiUrl = 'http://localhost:3000/api/tareas';
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
+  private getHeaders() {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
 
-getTasks(): Observable<any> {
-return this.http.get(this.apiUrl);
-}
+  getTasks(): Observable<any> {
+    return this.http.get(this.apiUrl, { headers: this.getHeaders() });
+  }
 
-createTask(task: any): Observable<any> {
-return this.http.post(this.apiUrl, task);
-}
+  createTask(task: any): Observable<any> {
+    return this.http.post(this.apiUrl, task, { headers: this.getHeaders() });
+  }
 
-deleteTask(id: number): Observable<any> {
-return this.http.delete(this.apiUrl + '/' + id);
-}
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+  }
 
-updateTask(id: number, task: any): Observable<any> {
-return this.http.put(this.apiUrl + '/' + id, task);
-}
+  updateTask(id: number, task: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, task, { headers: this.getHeaders() });
+  }
 }
